8fcbf5cd238d2b0ce7f88c7da647e959cd0f7e0c
Vamsi D <vamsi.akirah@gmail.com>
2021-06-10 16:47:22 +0530

    init

---
+++ plugins.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
+
+// default export should be an array of plugins
+module.exports = [nodeExternalsPlugin()];
